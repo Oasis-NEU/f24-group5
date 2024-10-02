1788b30f5efab69647348fe3884983c72942a759
pdsl2005 <pdsl2005@gmail.com>
2024-10-02 16:09:54 -0400

    added sound recognition

--- main.py
+++ main.py
@@ -1,20 +1,55 @@
+## API Frameworks
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
 import asyncio
+print("HI")
+## Audio Processing Libraries
+import pyaudio as pa
 import torch
 import speech_recognition as sr
-  
-# Create a Recognizer instance
-recognizer = sr.Recognizer()
+import numpy as np
 
-# Capture audio input from the microphone
-with sr.Microphone() as source:
- print("Speak something...")
- audio_data = recognizer.listen(source)
+## NLP Libraries
+from tensorflow import keras
 
-# Perform speech recognition using Google Web Speech API
-try:
- text = recognizer.recognize_google(audio_data)
- print("You said:", text)
-except sr.UnknownValueError:
- print("Sorry, could not understand audio.")
-except sr.RequestError as e:
- print("Error: Could not request results from Google Speech Recognition service;")
+#from os import path
+#audio_file= path.join(path.dirname(path.realpath(__file__)), "tests_english.wav")
+def speech_to_text_alpha1():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)    
+        print("Speak something...")
+        audio_data = r.listen(source)
+        try: 
+            text = r.recognize_whisper(audio_data)
+            print(text)
+        except sr.UnknownValueError:
+            print("Failed to transcribe")
+        except sr.RequestError as e:
+            print(f"Could not request results from service: {e}")
+        return text
+
+def speech_to_text_final(audio_file):
+    with sr.AudioFile(audio_file) as source:
+        audio = r.record(source)  # read the entire audio file
+        try: 
+            text = r.recognize_whisper(audio)
+            print(text)
+        except sr.UnknownValueError:
+            print("Failed to transcribe")
+        except sr.RequestError as e:
+            print(f"Could not request results from service: {e}")
+
+
+
+
+
+#class SoundFile(BaseModel):
+ #   name: str
+  #  sound_bite # type: ignore
+    
+#app = FastAPI()
+
+#@app.post("/SoundFile/")
+#async def create_sb():
+ #   return None
