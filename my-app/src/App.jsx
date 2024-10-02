a7169002ac8bf412bf0ee90eef64fbd90f5210bc
Rudra Parvate <rudra@parvate.com>
2024-10-02 18:15:56 -0400

    changed links

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -9,10 +9,10 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://www.google.com/search?q=rudra+parvate&client=firefox-b-1-d&sca_esv=17288cef834a1941&ei=rsT9Ztn4Ooeq5NoPk_3h-Qg&ved=0ahUKEwjZh5uT2_CIAxUHFVkFHZN-OI8Q4dUDCA8&uact=5&oq=rudra+parvate&gs_lp=Egxnd3Mtd2l6LXNlcnAiDXJ1ZHJhIHBhcnZhdGUyBRAhGKABMgUQIRigATIFECEYoAEyBRAhGKABMgUQIRigATIFECEYnwUyBRAhGJ8FMgUQIRifBTIFECEYnwUyBRAhGJ8FSIUlUI4FWNwicAR4ApABA5gB3AGgAaASqgEGMTAuOS4yuAEDyAEA-AEBmAIWoALoD6gCCsICBBAAGEfCAhYQLhiABBixAxjRAxhDGIMBGMcBGIoFwgILEAAYgAQYsQMYgwHCAggQABiABBixA8ICJRAuGIAEGLEDGNEDGEMYgwEYxwEYigUYlwUY3AQY3gQY4ATYAQHCAhYQABgDGLQCGOUCGOoCGIwDGI8B2AECwgIFEC4YgATCAg4QABiABBixAxiDARiKBcICDhAuGIAEGLEDGIMBGIoFwgIREC4YgAQYsQMY0QMYgwEYxwHCAg4QLhiABBixAxjRAxjHAcICFBAuGIAEGJcFGNwEGN4EGOAE2AEBwgIQEC4YgAQYsQMYQxiDARiKBcICChAAGIAEGEMYigXCAg4QLhiABBjHARiOBRivAcICCxAuGIAEGNEDGMcBwgIKEC4YgAQYQxiKBcICCBAuGIAEGLEDwgIfEC4YgAQYsQMYQxiDARiKBRiXBRjcBBjeBBjgBNgBAcICEBAuGIAEGNEDGEMYxwEYigXCAhMQLhiABBixAxhDGIMBGNQCGIoFwgINEC4YgAQYsQMYQxiKBcICEBAAGIAEGLEDGEMYgwEYigXCAg0QABiABBixAxhDGIoFwgIFEAAYgATCAisQLhiABBhDGIoFGJcFGNwEGN4EGOAEGPQDGPEDGPUDGPYDGPcDGPgD2AEBwgIHEC4YgAQYCsICCxAuGIAEGMcBGK8BwgIIEAAYFhgeGA_CAgsQABiABBiGAxiKBcICCBAAGIAEGKIEwgIIEAAYogQYiQXCAgYQABgWGB7CAggQLhgWGAoYHpgDAogGAZAGCLoGBggBEAEYFLoGBAgCGAqSBwYxMi44LjKgB5KWAg&sclient=gws-wiz-serp" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://www.linkedin.com/in/rudra-parvate/" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
