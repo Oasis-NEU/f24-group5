f0b5cef486a5a19ebd03d628152806c2ea0d79cb
Rudra Parvate <rudra@parvate.com>
2024-10-02 19:27:01 -0400

    deleted useless imports

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,17 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from "rsuite";
 
 
 function StartRecordButton() {
   return (
-  <button style={{width : '175px'}}
-          color="red">
+  <Button style={{width : '175px'}} color="red" appearance="primary">
     <h2>
       Start
     </h2>
-  </button>
+  </Button>
   )
 }
 
