805b6277bb4d2e2f84938ec46e5a9f1dfa88dddb
Rudra Parvate <rudra@parvate.com>
2024-11-16 13:32:09 -0500

    hecka

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,61 +1,272 @@
 import * as React from 'react';
-// import { useState } from 'react'
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import CssBaseline from '@mui/material/CssBaseline';
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Button from '@mui/material/Button';
-import { Snackbar } from '@mui/material';
+import { Typography } from '@mui/material';
+import Divider from '@mui/material/Divider';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import Stack from '@mui/material/Stack';
+import Paper from '@mui/material/Paper';
+import Fade from '@mui/material/Fade';
+import TrapFocus from '@mui/material/Unstable_TrapFocus';
+// import InterpreterModeIcon from '@mui/icons-material/InterpreterMode';
+// import { spacing } from '@mui/system';
 
+// var cookiesAccepted = false;
 
-function AlertDialogue() {
-  const [open, setOpen] = React.useState(false);
-  const handleClickOpen = () => {
-    setOpen(true);
+// function CookieHandler() {
+//   if (!cookiesAccepted) {
+//     return <AlertDialogue/>
+//   }
+// }
+
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
   };
 
-  const handleClose = () => {
-    setOpen(false);
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="fixed">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: 'block', md: 'none' } }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography sx={{ textAlign: 'center' }}>{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography sx={{ textAlign: 'center' }}>{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+function CookiesBanner() {
+  const [bannerOpen, setBannerOpen] = React.useState(true);
+
+  const closeBanner = () => {
+    setBannerOpen(false);
   };
 
   return (
     <React.Fragment>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Open Cookie Settings
-      </Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          {"Let us use cookies?"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            We haven&apos;t technically implemented cookies, but we could like you to allow us to use 
-            &quot;cookies&quot; anyways
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button href='https://www.google.com'>Disagree</Button>
-          <Button openAgree={true} onClick={handleClose} autoFocus>
-            Agree
-          </Button>
-          <Snackbar
-            open={open}
-            onClose={handleClose}
-            message="ACCEPT THE COOKIES"
-          />
-        </DialogActions>
-      </Dialog>
+      <CssBaseline />
+      <AppBar position="fixed" component="nav">
+        <Toolbar>
+          <IconButton size="large" edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Container component="main" sx={{ pt: 3 }}>
+        <Toolbar />
+        <Typography sx={{ marginBottom: 2 }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+          imperdiet.
+        </Typography>
+        <Typography sx={{ marginBottom: 2 }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+          imperdiet.
+        </Typography>
+      </Container>
+      <TrapFocus open disableAutoFocus disableEnforceFocus>
+        <Fade appear={false} in={bannerOpen}>
+          <Paper
+            role="dialog"
+            aria-modal="false"
+            aria-label="Cookie banner"
+            square
+            variant="outlined"
+            tabIndex={-1}
+            sx={{
+              position: 'fixed',
+              bottom: 0,
+              left: 0,
+              right: 0,
+              m: 0,
+              p: 2,
+              borderWidth: 0,
+              borderTopWidth: 1,
+            }}
+          >
+            <Stack
+              direction={{ xs: 'column', sm: 'row' }}
+              sx={{ justifyContent: 'space-between', gap: 2 }}
+            >
+              <Box
+                sx={{ flexShrink: 1, alignSelf: { xs: 'flex-start', sm: 'center' } }}
+              >
+                <Typography sx={{ fontWeight: 'bold' }}>
+                  This website uses cookies
+                </Typography>
+                <Typography variant="body2">
+                  example.com relies on cookies to improve your experience.
+                </Typography>
+              </Box>
+              <Stack
+                direction={{
+                  xs: 'row-reverse',
+                  sm: 'row',
+                }}
+                sx={{
+                  gap: 2,
+                  flexShrink: 0,
+                  alignSelf: { xs: 'flex-end', sm: 'center' },
+                }}
+              >
+                <Button size="small" onClick={closeBanner} variant="contained">
+                  Allow all
+                </Button>
+                <Button size="small" onClick={closeBanner}>
+                  Reject all
+                </Button>
+              </Stack>
+            </Stack>
+          </Paper>
+        </Fade>
+      </TrapFocus>
     </React.Fragment>
   );
 }
+
 
 function GetStartedButton() {
   return (
@@ -70,10 +281,12 @@ function App() {
 
   return (
     <>
-    <AlertDialogue/>
+    <ResponsiveAppBar/>
+    <div/>
+    <CookiesBanner/>
       <div>
         <a href="https://www.linkedin.com/in/rudra-parvate/" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="InterpreterModeIcon" alt="SPEAKER" />
         </a>
       </div>
       <h1>Welcome to Speaker!</h1>
@@ -87,8 +300,11 @@ function App() {
         Click on the React logo
       </p>      
       <div>
+        <Divider />
         <p className = "read-the-docs">
+          <Typography variant='subtitle1'>
           Noah Cheng&apos;s real name is Joe Jr.
+          </Typography>
         </p>
       </div>
     </>
