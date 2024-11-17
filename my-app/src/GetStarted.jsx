4934dce59da2bb12a132c295522ae4248bd827fd
jchengjr <noahwcheng@gmail.com>
2024-11-17 13:43:22 -0500

    cleanup

---
+++ my-app/src/GetStarted.jsx
@@ -0,0 +1,7 @@
+import { TextField } from "@material-ui/core";
+
+<body>
+<TextField id="outlined-basic" label="Outlined" variant="outlined" />
+<TextField id="filled-basic" label="Filled" variant="filled" />
+<TextField id="standard-basic" label="Standard" variant="standard" />
+</body>
