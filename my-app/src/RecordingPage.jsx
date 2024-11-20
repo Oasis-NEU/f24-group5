47ba1473c31d03c864e5b6c9cf2282298d3b04a5
Rudra Parvate <rudra@parvate.com>
2024-11-20 02:32:04 -0500

    somehow working with wav file outputted

--- my-app/src/RecordingPage.jsx
+++ my-app/src/RecordingPage.jsx
@@ -1,17 +1,35 @@
 import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
-import './App.css'
-import { Typography } from '@mui/material';
-
+import './App.css';
+import { Typography, Button } from '@mui/material';
+import { ReactMediaRecorder } from 'react-media-recorder';
 
 export default function RecordingPage() {
   return (
     <>
-    <div>
-      <Typography variant="h6" sx={{ my: 2 }} align='center'>
-        This is a test.
-      </Typography>
-    </div>
+      <CssBaseline />
+      <div>
+        <Typography variant="h7" sx={{ my: 2 }} align='center'>
+          Begin recording on your own time (beginning a new recording will stop the current one).
+        </Typography>
+        <ReactMediaRecorder
+          audio
+          mimeType="audio/wav"
+          render={({ startRecording, stopRecording, mediaBlobUrl }) => (
+            <div style={{ textAlign: 'center' }}>
+              <Button variant="contained" color="primary" onClick={startRecording}>
+                Start Recording
+              </Button>
+              <Button variant="contained" color="secondary" onClick={stopRecording} sx={{ ml: 2 }}>
+                Stop Recording
+              </Button>
+              {mediaBlobUrl && (
+                <audio src={mediaBlobUrl} controls style={{ display: 'block', marginTop: '20px' }} />
+              )}
+            </div>
+          )}
+        />
+      </div>
     </>
-  )
+  );
 }
